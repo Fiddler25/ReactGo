769beb4d24be7d0f2a072f988ab64294c2788691
AtsushiKanayama <rager.mka.ti@icloud.com>
2021-01-17 19:39:10 +0900

    CORS対応

--- api/src/main.go
+++ api/src/main.go
@@ -2,11 +2,17 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/gin-contrib/cors"
 	"routes"
 )
 
 func main() {
 	r := gin.Default()
+
+    config := cors.DefaultConfig()
+    config.AllowOrigins = []string{"http://localhost:4000"}
+	r.Use(cors.New(config))
+
 	r.GET("/", routes.Index)
 
 	r.Run()

4c9b5b79b4083f6029c7e2f20793d0a31d8ef661
AtsushiKanayama <rager.mka.ti@icloud.com>
2021-01-28 06:50:20 +0900

    sessions導入と動作確認

--- api/src/main.go
+++ api/src/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/gin-contrib/cors"
+	"github.com/gin-contrib/sessions"
+    "github.com/gin-contrib/sessions/cookie"
 	"routes"
 )
 
@@ -12,6 +14,20 @@ func main() {
     config := cors.DefaultConfig()
     config.AllowOrigins = []string{"http://localhost:4000"}
 	r.Use(cors.New(config))
+
+	store := cookie.NewStore([]byte("secret"))
+    r.Use(sessions.Sessions("mysession", store))
+ 
+    r.GET("/hello", func(c *gin.Context) {
+        session := sessions.Default(c)
+ 
+        if session.Get("hello") != "world" {
+            session.Set("hello", "world")
+            session.Save()
+        }
+ 
+        c.JSON(200, gin.H{ "hello": session.Get("hello") })
+    })
 	
 	r.GET("/", routes.Index)
 	r.POST("/login", routes.Login)

1dbd6132092486df9d337a5b1e7f2cdb08bbdb2f
AtsushiKanayama <rager.mka.ti@icloud.com>
2021-01-29 06:29:20 +0900

    疎通確認

--- api/src/main.go
+++ api/src/main.go
@@ -1,15 +1,55 @@
 package main
 
 import (
+	"database/sql"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 	"github.com/gin-contrib/cors"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-contrib/sessions/cookie"
 	"routes"
+	. "fmt"
+	"log"
+	"os"
 )
+
+type Person struct {
+    ID   int
+    Name string
+}
 
 func main() {
 	r := gin.Default()
+
+	err := godotenv.Load()
+    if err != nil {
+        log.Fatal(err)
+	}
+
+	db, err := sql.Open("mysql", os.Getenv("DB_USER") + ":" + os.Getenv("DB_PASSWORD") + "@/" + os.Getenv("DB_NAME"))
+	log.Println("Connected to mysql.")
+
+	if err != nil {
+        log.Fatal(err)
+	}
+	defer db.Close()
+
+	rows, err := db.Query("SELECT * FROM users")
+    defer rows.Close()
+    if err != nil {
+        log.Fatal(err)
+    }
+
+	for rows.Next() {
+        var person Person
+        err := rows.Scan(&person.ID, &person.Name)
+
+        if err != nil {
+            log.Fatal(err)
+        }
+        Println(person.ID, person.Name)
+	}
 
 	config := cors.DefaultConfig()
 	config.AllowOrigins = []string{"http://localhost:4000"}
